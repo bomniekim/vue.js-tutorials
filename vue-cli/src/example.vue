076f2930e0db1e82630b8409803c7ccd53896d80
Bomin Kim <bomnie.kim@gmail.com>
2020-05-26 15:01:08 +0900

    file-name.vue: 싱글 파일 컴포넌트(html/js/css를 한 파일에 관리)

--- vue-cli/src/example.vue
+++ vue-cli/src/example.vue
@@ -1,11 +1,13 @@
 
 // html, javascript, css를 한 파일에 관리하는 싱글 파일 컴포넌트
-// 단축키: 'vue' + tab
+// 기본구조 단축키: 'vue' + tab
 
 
 var appHeader = {
-    template: '<div>header</div>',
-   
+  template: '<div>header</div>',
+  methods: {
+    addNum: function() {}
+  }
 }
 
 <template>
@@ -15,6 +17,7 @@ var appHeader = {
 
 <script>
 export default {
+  // ES6: 모듈화
   // javascript - 인스턴스 옵션
   methods: {
     addNum: function() {}
