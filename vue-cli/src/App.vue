0d38cadeaf4a55f1df6e20970bf7fc84faf3b9cd
Bomin Kim <bomnie.kim@gmail.com>
2020-05-26 20:12:20 +0900

    싱글 파일 컴포넌트에서 props/event emit 사용 및 구현하기

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,11 +1,48 @@
 <template>
-  <!-- template root는 무조건 하나의 element만 있어야 함 -->
-  <div>app</div>
+  <!-- template root는 무조건 하나의 HTML element만 있어야 함 -->
+  <div>
+    {{ str }}
+    <!-- <app-header v-bind:컴포넌트의 프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
+  </div>
 </template>
 
 <script>
-export default {};
+// 1) 컴포넌트의 내용을 변수에 담음
+import AppHeader from "./components/AppHeader.vue";
+
+/*
+var AppHeader = {
+  template: '<header><h2>Header</h2></header>'
+}*/
+
+// root component
+export default {
+  // 우선규칙 A
+  // 컴포넌트의 data 는 객체(object)를 반환하는 함수!
+  // 컴포넌트를 재사용하겠다는 의지, 확률이 높기 때문에 여러 개의 컴포넌트에서 동일한 값을 참조하면 안됨
+  data: function() {
+    return {
+      str: "data descending"
+    };
+  },
+
+  components: {
+    "app-header": AppHeader // 2) 변수를 연결
+  },
+
+  methods: {
+    renewStr: function() {
+      this.str = "renew function is implemented";
+    }
+  }
+};
+
+// new Vue({
+//   data: {
+//     str: 'hi vue!'
+//   }
+// })
 </script>
 
-<style>
-</style>
+<style></style>
