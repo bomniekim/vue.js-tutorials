5c003a65ba5d3cc79139fe8e31c9c2d55f5da407
Bomin Kim <bomnie.kim@gmail.com>
2020-05-28 23:47:59 +0900

    localStorage.setItem(key, value)

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="inputBox shadow">
+    <!-- v-model : two way data-binding (화면단과 script단의 동기화)-->
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <!-- .enter : event modifier -->
+
+    <!-- span으로 button customizing -->
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fas fa-plus addBtn"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    };
+  },
+  methods: {
+    addTodo: function() {
+      // 저장하는 로직 api - localStorage.setItem(key, value);
+      if (this.newTodoItem !== "") {
+        // * 확인 필요...
+        localStorage.setItem(this.newTodoItem, this.newTodoItem);
+        // 개발자 도구 > Application > local storage에서 확인
+        this.clearInput();
+      }
+    },
+    // 메서드 분할
+    clearInput: function() {
+      this.newTodoItem = ""; // input 값 초기화 꼼수
+    }
+  }
+};
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+  height: 40px;
+  line-height: 40px;
+}
+/* span */
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
