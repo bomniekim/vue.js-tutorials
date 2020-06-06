7985c1f342eeff2936d2a7474973448253b1df13
Bomin Kim <bomnie.kim@gmail.com>
2020-06-06 19:55:16 +0900

    modal overwrite

--- todo-refactoring/src/components/TodoInput.vue
+++ todo-refactoring/src/components/TodoInput.vue
@@ -8,14 +8,30 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="fas fa-plus addBtn"></i>
     </span>
+
+    <Modal v-if="showModal" @close="showModal = false">
+      <!--
+      you can use custom content here to overwrite
+      default content
+      -->
+      <h3 slot="header">
+        경고!
+        <i class="closeModalBtn fas fa-times" @click="showModal = false"></i>
+      </h3>
+
+      <div slot="body">값을 입력해주세요.</div>
+      <div slot="footer">vue.js modal</div>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
 export default {
   data: function() {
     return {
       newTodoItem: "",
+      showModal: false
     };
   },
   methods: {
@@ -23,17 +39,25 @@ export default {
       if (this.newTodoItem !== "") {
         this.$emit("addTodoItem", this.newTodoItem); // 하위 컴포넌트에서의 event emit
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     // 메서드 분할
     clearInput: function() {
       this.newTodoItem = ""; // input 값 초기화 꼼수
-    },
+    }
   },
+  components: {
+    Modal: Modal
+  }
 };
 </script>
 
 <style scoped>
+input {
+  width: 80%;
+}
 input:focus {
   outline: none;
 }
@@ -61,5 +85,9 @@ input:focus {
 .addBtn {
   color: white;
   vertical-align: middle;
+}
+
+.closeModalBtn {
+  color: #42b983;
 }
 </style>
